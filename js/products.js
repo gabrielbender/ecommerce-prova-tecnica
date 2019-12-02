ef883f90e42492d48c2ac2a1c4dd728dbb079712
gabriel.bender <gabriel.bender@b2breservas.com.br>
2019-12-02 17:51:46 -0300

    second

--- js/products.js
+++ js/products.js
@@ -1,14 +1,65 @@
+
 $( document ).ready(function() {
+    var currentPage = 1;
     
-    let dados = {};
+    $.ajax({
+        url:"https://raw.githubusercontent.com/gabrielbender/ecommerce-prova-tecnica/master/mock-products.json",
+        method:"GET"
+    }).done(function(resposta){
+        var dados = JSON.parse(resposta);
+        var pagesQtd = Math.ceil(dados.products.length / 20)+1; 
+        var productList = $('.product-list');
+        dados.products.forEach(function(item,index){
+            var produto = 
+            `
+            <li class="product-item" data-number=${index + 1} data-page=${Math.floor((index) / 20)+1}>
+                <div class="product-wrapper">
+                    <a href="#">
+                        <div class="product-image-wrapper">
+                            <img 
+                                src="${item.image}"
+                                class="product-image" 
+                                alt="Foto do produto ${item.name}" 
+                            />
+                        </div>
+                        <div class="product-info">
+                            <h2 class="product-name">${item.name}</h2>
+                            <p class="product-value">R$ ${item.price}</p>
+                        </div>
+                    </a>
+                </div>
+            </li>
+            `;
+            
+            productList.append(produto);
+            
+        });
 
-    fetch('mock-produtos.json', {mode: 'no-cors'})
-        .then(res => res.json())
-        .then(res => dados = res)
-        .catch();
+        pagination(currentPage);
 
-    console.log(dados);
+        for(let i = 1; i < pagesQtd; i++){
+            $('.last-page').before(`
+                <li class="page-item"><a class="page-link" href="#" data-page="${i}">${i}</a></li>
+            `);
+        }
 
+        $('.last-page .page-link').attr("data-page", pagesQtd);
+
+        function pagination(page){
+            $(".product-item").css("display","none");
+            $(".product-item[data-page="+page+"] ").css("display","block");
+            console.log(page);
+        }
     
+        $('.page-link').on("click", function(event){
+            event.preventDefault();
+            pagination($(this).attr('data-page'));
+        });
 
+    });
+    
+    
 });
+
+
+
